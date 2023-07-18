c816cf0b52a3e41d091d213b2bb599d1438e869f
eman <emanadelibrahim2@gmail.com>
2023-07-18 21:46:35 +0300

    build tab component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Tabs from "./components/Tabs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Tab Component</div>
+      <Tabs>
+        <Tabs.TabList>
+          <Tabs.Tab>One</Tabs.Tab>
+          <Tabs.Tab>Two</Tabs.Tab>
+          <Tabs.Tab>Three</Tabs.Tab>
+        </Tabs.TabList>
+        <Tabs.TabPanels>
+          <Tabs.Panel>
+            <p>Tab one content!</p>
+          </Tabs.Panel>
+          <Tabs.Panel>
+            <p>Tab two content!</p>
+          </Tabs.Panel>
+          <Tabs.Panel>
+            <p>Tab three content!</p>
+          </Tabs.Panel>
+        </Tabs.TabPanels>
+      </Tabs>
+    </>
   );
 }
 
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,135 @@
+import React, { ReactElement } from "react";
+export type TabsPropType = {
+  children: React.ReactNode;
+};
+export type TabsContextType = {
+  activeTab?: number;
+  onChange: (key: number) => void;
+};
+export const TabsContext = React.createContext<TabsContextType | null>(null);
+export function useTabsContext() {
+  const context = React.useContext(TabsContext);
+  if (!context) return;
+  return context;
+}
+
+const Tabs = ({ children }: TabsPropType) => {
+  const [activeTab, setActiveTab] = React.useState<number>(0);
+  const onChange = React.useCallback(
+    (tabKey: number) => setActiveTab(tabKey),
+    []
+  );
+  const value = React.useMemo(
+    () => ({ activeTab, onChange }),
+    [activeTab, onChange]
+  );
+  return (
+    <TabsContext.Provider value={value}>
+      <div className="tabs">{children}</div>
+    </TabsContext.Provider>
+  );
+};
+type PropType = {
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+type TabProps = {
+  onClick?: () => void;
+  children: ReactElement;
+};
+const TabList = ({ children }: PropType) => {
+  const { onChange } = useTabsContext();
+  const tabList = React.Children.map(children, (child, index) => {
+    if (!React.isValidElement<TabProps>(child)) {
+      return null;
+    }
+    return React.cloneElement(child, {
+      onClick: () => onChange(index),
+    });
+  });
+  return <div className="tab-list">{tabList}</div>;
+};
+const Tab = ({ children, onClick }: PropType) => {
+  return (
+    <div className="tab" onClick={onClick}>
+      {children}
+    </div>
+  );
+};
+const TabPanels = ({ children }: PropType) => {
+  const { activeTab } = useTabsContext();
+  const tabPanels = React.Children.map(children, (child, index) => {
+    if (!React.isValidElement(child)) {
+      return null;
+    }
+    return activeTab === index ? child : null;
+  });
+  return <div className="tab-panels">{tabPanels}</div>;
+};
+const Panel = ({ children }: PropType) => {
+  return <div className="tab-panel">{children}</div>;
+};
+Tabs.TabList = TabList;
+Tabs.Tab = Tab;
+Tabs.TabPanels = TabPanels;
+Tabs.Panel = Panel;
+export default Tabs;
+
+//   function handleTabChange(index) {
+//     setActiveTab(index);
+//   }
+
+//   const tabs = React.Children.map(children, (child, index) => {
+//     return React.cloneElement(child, {
+//       index,
+//       isActive: index === activeTab,
+//       onTabChange: handleTabChange,
+//     });
+//   });
+
+//   const tabContent = React.Children.map(children, (child, index) => {
+//     return React.cloneElement(child, {
+//       isActive: index === activeTab,
+//     });
+//   });
+
+//   return (
+//     <div>
+//       <div>{tabs}</div>
+//       <div>{tabContent}</div>
+//     </div>
+//   );
+// }
+// export default Tabs;
+
+//the name of this context will be DataContext
+// const DataContext = createContext({});
+
+// function Tab({ id, children }) {
+//   //extract the 'setActiveTabID` method from the DataContext state.
+//   const [, setActiveTabID] = useContext(DataContext);
+//   return (
+//     <div>
+//       <div onClick={() => setActiveTabID(id)}>{children}</div>
+//     </div>
+//   );
+// }
+// function TabPanel({ whenActive, children }) {
+//   //get the 'activeTabID' state from DataContext.
+//   const [activeTabID] = useContext(DataContext);
+//   return <div>{activeTabID === whenActive ? children : null}</div>;
+// }
+
+// function TabSwitcher(props) {
+//   const [activeTabID, setActiveTabID] = useState("a");
+//   //since this component will provide data to the child components, we will use DataContext.Provider
+//   return (
+//     <DataContext.Provider value={[activeTabID, setActiveTabID]}>
+//       {props.children}
+//     </DataContext.Provider>
+//   );
+// }
+
+// export default TabSwitcher;
+// export { Tab, TabPanel };
