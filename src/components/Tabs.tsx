fd000ef2514a6eaf15b4a2edb8d514067724b8e9
eman <emanadelibrahim2@gmail.com>
2023-07-18 22:06:02 +0300

    clean code

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -17,7 +17,6 @@ const Tabs = ({ children }: TabsPropType) => {
     </TabsContext.Provider>
   );
 };
-
 const TabList = ({ children }: PropType) => {
   const handleChange = useTabsContext();
   const tabList = React.Children.map(children, (child, index) => {
@@ -55,61 +54,3 @@ Tabs.Tab = Tab;
 Tabs.TabPanels = TabPanels;
 Tabs.Panel = Panel;
 export default Tabs;
-
-//   function handleTabChange(index) {
-//     setActiveTab(index);
-//   }
-
-//   const tabs = React.Children.map(children, (child, index) => {
-//     return React.cloneElement(child, {
-//       index,
-//       isActive: index === activeTab,
-//       onTabChange: handleTabChange,
-//     });
-//   });
-
-//   const tabContent = React.Children.map(children, (child, index) => {
-//     return React.cloneElement(child, {
-//       isActive: index === activeTab,
-//     });
-//   });
-
-//   return (
-//     <div>
-//       <div>{tabs}</div>
-//       <div>{tabContent}</div>
-//     </div>
-//   );
-// }
-// export default Tabs;
-
-//the name of this context will be DataContext
-// const DataContext = createContext({});
-
-// function Tab({ id, children }) {
-//   //extract the 'setActiveTabID` method from the DataContext state.
-//   const [, setActiveTabID] = useContext(DataContext);
-//   return (
-//     <div>
-//       <div onClick={() => setActiveTabID(id)}>{children}</div>
-//     </div>
-//   );
-// }
-// function TabPanel({ whenActive, children }) {
-//   //get the 'activeTabID' state from DataContext.
-//   const [activeTabID] = useContext(DataContext);
-//   return <div>{activeTabID === whenActive ? children : null}</div>;
-// }
-
-// function TabSwitcher(props) {
-//   const [activeTabID, setActiveTabID] = useState("a");
-//   //since this component will provide data to the child components, we will use DataContext.Provider
-//   return (
-//     <DataContext.Provider value={[activeTabID, setActiveTabID]}>
-//       {props.children}
-//     </DataContext.Provider>
-//   );
-// }
-
-// export default TabSwitcher;
-// export { Tab, TabPanel };
